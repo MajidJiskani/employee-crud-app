9216ccf9868b6f9269d58c4d7a7e6ec3e351d278
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2026-02-11 11:27:59 +0500

    Remove unnecessary files

--- tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-app.get('/a', async (req, res) => {
-    let edetails = await Edetails.findOne({})
-    console.log(edetails)
-     res.json({Emp_id:edetails.Emp_id, Join_Date: edetails.Join_Date, Salary: edetails.Salary, Employee_name: edetails.Employee_name})
-})
